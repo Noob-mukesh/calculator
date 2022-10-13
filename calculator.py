505c90f2be1f7da148f2f771925d3e70cdce9fcd
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-10-13 17:50:10 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,32 @@
+mukesh='Simple Python Calculator by [MUKESH](https://t.me/itz_mst_boi)\n'
+
+mukesh2=mukesh.center(50)
+print(mukesh2)
+
+
+a=float(input('ENTER YOUR 1ST NUMBER dude:= '))
+
+b=float(input('ENTER  YOUR 2ND NUMBER dude:='))
+ 
+c=input('Enter operation you want to calculate\n SUPPORTING OPERATION ARE : +,-,*,**,and /')
+
+# to perform addition
+if '+' in c:
+    print('Addition of two  is',a+b)
+    # to perform subtraction
+elif  '-' in c:
+       print('Subtraction of two numbers is',a-b)
+# to perform multiplication
+elif '*' in c:
+    print('Multiplication of two numbers is',a*b)
+    # to perform division
+elif '/' in c:
+    print('Division of two numbers is',a/b)
+ 
+elif '%' in c:
+     print('reminder of given number  is ',a%b)
+
+elif '**' in c:
+    print('power of given number is',a**b)
+else:
+    print("wrong operator lol")
