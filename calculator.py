9c873cff3512efe672e16c65dbb639ca54869d44
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-12-02 03:34:27 +0000

    updated text format

--- calculator.py
+++ calculator.py
@@ -1,32 +1,35 @@
-mukesh='Simple Python Calculator by [MUKESH](https://t.me/itz_mst_boi)\n'
+mukesh = 'Simple Python Calculator by [MUKESH](https://t.me/itz_mst_boi)\n'
 
-mukesh2=mukesh.center(50)
+mukesh2 = mukesh.center(50)
 print(mukesh2)
 
 
-a=float(input('ENTER YOUR 1ST NUMBER dude:= '))
+a = float(input('ENTER YOUR 1ST NUMBER dude:= '))
 
-b=float(input('ENTER  YOUR 2ND NUMBER dude:='))
- 
-c=input('Enter operation you want to calculate\n SUPPORTING OPERATION ARE : +,-,*,**,and /')
+b = float(input('ENTER  YOUR 2ND NUMBER dude:='))
+
+c = input(
+    'Enter operation you want to calculate\n SUPPORTING OPERATION ARE : +,-,*,**,and /')
 
 # to perform addition
 if '+' in c:
-    print('Addition of two  is',a+b)
+    print('Addition of two  is', a+b)
     # to perform subtraction
-elif  '-' in c:
-       print('Subtraction of two numbers is',a-b)
+elif '-' in c:
+    print('Subtraction of two numbers is', a-b)
 # to perform multiplication
 elif '*' in c:
-    print('Multiplication of two numbers is',a*b)
+    print('Multiplication of two numbers is', a*b)
     # to perform division
 elif '/' in c:
-    print('Division of two numbers is',a/b)
- 
-elif '%' in c:
-     print('reminder of given number  is ',a%b)
+    print('Division of two numbers is', a/b)
+    #  to get reminder
 
+elif '%' in c:
+    print('reminder of given number  is ', a % b)
+#  to find power
 elif '**' in c:
-    print('power of given number is',a**b)
+    print('power of given number is', a**b)
+    #  else ur lol
 else:
     print("wrong operator lol")
